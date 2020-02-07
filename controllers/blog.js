de890e42cda682288a0150fc44d3f058bedd5703
Yan Yan <yyan@c2r2p13.hive.fi>
2020-02-07 15:48:58 +0200

    put and delete request added

--- controllers/blog.js
+++ controllers/blog.js
@@ -11,6 +11,45 @@ blogRouter.get('/', async (request, response) => {
 	}
 })
 
+blogRouter.get('/:id', async (request, response)=>{
+	try{
+		const blog = await Blog.findById(request.params.id)
+		if (blog){
+			response.json(blog.toJSON())
+		}else{
+			response.status(404).end()
+		}
+	}catch(exception){
+		console.log(exception)
+	}
+})
+
+blogRouter.delete('/:id', async(request, response)=>{
+	try{
+		await Blog.findByIdAndRemove(request.params.id)
+		response.status(204).end()
+	}catch(exception){
+		console.log(exception)
+	}
+
+})
+
+blogRouter.put('/:id', async(request, response)=>{
+	const body = request.body
+	const blog ={
+		'title': body.title,
+		'author': body.author,
+		'url': body.url,
+		'likes': body.likes
+	}
+	try{
+		const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+		response.json(updatedBlog.toJSON())
+	}catch(exception){
+		console.log(exception)
+	}
+})
+
 blogRouter.post('/', (request, response) => {
 	try{
 		if (!request.body.likes){
