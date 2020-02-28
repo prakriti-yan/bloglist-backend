b7685b3f926aac98137f5ed3fc473ba03e8242ef
Yan Yan <yyan@c2r2p9.hive.fi>
2020-02-28 14:07:17 +0200

    build added

---
+++ build/precache-manifest.d1009be3b72c00bd48f884512e5408bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb4d43fb94c37edea17127e24b30d10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70e5990ec8b3afe00559",
+    "url": "/static/css/main.6311c512.chunk.css"
+  },
+  {
+    "revision": "4852796a1ee29074186f",
+    "url": "/static/js/2.ed55c2c6.chunk.js"
+  },
+  {
+    "revision": "70e5990ec8b3afe00559",
+    "url": "/static/js/main.3591d559.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "65fe292c684195b11a1ddc787460a13c",
+    "url": "/static/media/pic2.65fe292c.jpg"
+  }
+]);
